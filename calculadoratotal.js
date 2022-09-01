7c16e1343e09ada193b6cef15476715d0bc61d8a
ericescobar2020 <ericescobar2020@outlook.com>
2022-08-31 21:01:36 -0300

    nuevo codigo process.args

--- calculadoratotal.js
+++ calculadoratotal.js
@@ -15,3 +15,35 @@ console.log(resultado1)
 
 const resultado2= dividir(3,0);
 console.log(resultado2) 
+////////////////////////
+
+const accion = process.argv[2];
+
+const num1= parseInt(process.argv[3])
+const num2=parseInt(process.argv[4])
+
+if(!accion|| !num1|| !num2) return; 
+
+switch(accion ){
+   
+    case "sumar":{
+        const resultado = sumar(num1,num2);
+        console.log(num1,"+",num2,"=",resultado)
+        break;
+    }
+    case "restar":{ 
+     const resultado = restar(num1,num2);
+    console.log(num1,"-",num2,"=",resultado)
+    break;
+    }
+    case "multiplicar":{ 
+        const resultado = multiplicar(num1,num2);
+       console.log(num1,"*",num2,"=",resultado)
+       break;
+       }
+     case "dividir":{ 
+          const resultado = dividir(num1,num2);
+          console.log(num1,"/",num2,"=",resultado)
+          break;
+       }
+    }
